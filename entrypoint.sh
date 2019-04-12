8503de70ea1073dd5dbc9617ca9d8a80f1f45ae7
Ba Huy Tran <tranbahuy@gmail.com>
2019-04-12 09:35:19 +0200

    Add files via upload

--- entrypoint.sh
+++ entrypoint.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
-sed -i 's/ENDPOINT_URL/'"${ENDPOINT_URL}"'/g' /usr/local/tomcat/webapps/ROOT/ep.js
+sed -i 's|ENDPOINT_URL|'"${ENDPOINT_URL}"'|g' /usr/local/tomcat/webapps/ROOT/ep.js
 sed -i 's/ADMINPASS/'"${ADMIN_PASS}"'/g' /usr/local/tomcat/conf/tomcat-users.xml
 sed -i 's/HOST_NAME/'"${HOST_NAME}"'/g' /usr/local/tomcat/webapps/ep/WEB-INF/connection.properties
 sed -i 's/PORT_NUM/'"${PORT_NUM}"'/g' /usr/local/tomcat/webapps/ep/WEB-INF/connection.properties
